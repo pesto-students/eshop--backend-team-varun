37ca99472e2604cc65d84a11cd5cda3be5c287f5
Shubham Sati <68639424+Shubham-Sati@users.noreply.github.com>
2023-03-16 08:29:50 +0530

    starting template server started and mongoDB connected

---
+++ config/connectDB.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  await mongoose
+    .connect(process.env.MONGO_URI)
+    .then((data) => {
+      //   console.log(`MongoDB connected to the server ${data.connection.host}`);
+      console.log(`MongoDB connected to the server`);
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+  console.log("Trying to reconnect.....");
+  connectDB();
+});
