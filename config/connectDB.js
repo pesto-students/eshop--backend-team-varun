1b7260e7a50fcb9f9c2dfc87012780c5929ef2db
Shubham Sati <68639424+Shubham-Sati@users.noreply.github.com>
2023-03-16 12:59:07 +0530

    updated DB connection file

--- config/connectDB.js
+++ config/connectDB.js
@@ -4,7 +4,6 @@ export const connectDB = async () => {
   await mongoose
     .connect(process.env.MONGO_URI)
     .then((data) => {
-      //   console.log(`MongoDB connected to the server ${data.connection.host}`);
       console.log(`MongoDB connected to the server`);
     })
     .catch((error) => {
