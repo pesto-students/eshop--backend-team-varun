c92d74a998c272602d0a7922ab60256a96ff19f5
Yogender <yogender9310@gmail.com>
2023-03-18 14:01:44 +0530

    Error handling modified

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const errorMiddleware = require("./middleware/error")
 
 
 app.use(express.json());
@@ -9,6 +10,9 @@ app.use(express.json());
 
 const product = require("./routes/productRoute");
 
+ //Middleware for Error
+
+ app.use(errorMiddleware);
 
 app.use("/api/v1",product);
 
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,15 @@ const dotenv =require("dotenv");
 const connectDatabase =require("./config/database");
 
 
+//handling Uncaught Exceptions Errors
+process.on("uncaughtException",(err)=>{
+    console.log(` Error : ${err.message}`);
+    console.log(`Shutting down the server due to Uncaught Exceptions`);
+ 
+  server.close(()=>{
+    process.exit(1);
+  });
+ });
 
 
 
@@ -13,11 +22,24 @@ const connectDatabase =require("./config/database");
 
  //connecting  to DataBase
  connectDatabase();
+ 
 
  const server = app.listen(process.env.PORT,()=>{
     console.log(`Sever is working   on http: //localhost ${process.env.PORT} `);
  });
 
+
+
+  // Unhandled Promise Rejections Errors
+
+process.on("unhandledRejection",(err)=>{
+    console.log(` Error : ${err.message}`);
+    console.log(`Shutting down the server due to unhandled promise Resjections`);
+ 
+  server.close(()=>{
+    process.exit(1);
+  });
+ });
 
 
 
