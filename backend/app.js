85e1c3d2c9e2c61128e84997a94dc118455759f8
Yogender <yogender9310@gmail.com>
2023-03-25 10:09:47 +0530

    Stipe payment gateWay created

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,10 @@
   const cookieParser = require("cookie-parser");
 
   const errorMiddleware = require("./middleware/error");
+  const dotenv = require("dotenv");
+
+  //config
+ dotenv.config({path:"backend/config/config.env"});
 
   app.use(express.json());
   
@@ -13,10 +17,12 @@
   const product = require("./routes/productRoute");
   const user = require("./routes/userRoute");
   const order = require("./routes/orderRoute");
+  const payment = require("./routes/paymentRoutes");
 
   app.use("/api/v1",product);
   app.use("/api/v1",user);
-app.use("/api/v1/",order);
+  app.use("/api/v1/",order);
+  app.use("/api/v1/",payment);
 
   //Middleware for Error
 
